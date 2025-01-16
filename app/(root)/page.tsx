e599eaa4247cdfe47efa53e91f664da88f95ed35
rahmon-najimov <najimov@epos.uz>
2025-01-16 16:03:27 +0500

    ADD: MobileNav and Navlinks

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,4 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
+import { auth } from "@/auth";
 
 const Home = async () => {
   const session = await auth();
@@ -9,15 +7,6 @@ const Home = async () => {
   return (
     <div>
       <h1 className="text-4xl font-black">Hello Next js course</h1>
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Log out</Button>
-      </form>
     </div>
   );
 };
